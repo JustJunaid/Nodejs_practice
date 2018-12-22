a54f3b70dbb53ca8fdfa3c80b4c20f8a0933cb08
Junaid <junaidchoudhary210@gmail.com>
2018-12-23 02:44:01 +0530

    assignment3

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.use('/home', (req, res, next) => {
-    console.log('This one is from first middleware')
+app.get('/users', (req, res) => {
     res.send('This is from home Route')
-    next()
 })
 
-app.use('/', (req, res, next) => {
-    console.log('This one is from second middleware')
-    res.send('<h1>This one is from second middleware</h1>')
+app.use('/', (req, res) => {
+    res.send('')
 })
 
 app.listen(3000)
