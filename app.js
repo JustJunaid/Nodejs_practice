1ab3e7eb03a36848eaa2e9f0998785758ef25787
Junaid <junaidchoudhary210@gmail.com>
2018-12-23 03:01:38 +0530

    completed assignment3 branch

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/users', (req, res) => {
-    res.send('This is from home Route')
+    res.sendFile(path.join(__dirname, 'views', 'users.html'))
 })
 
-app.use('/', (req, res) => {
-    res.send('')
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'home.html'))
+})
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(3000)
